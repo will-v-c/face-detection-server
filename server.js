22f42ff1fa333b966c5c90711d465a47c4135886
Willy-Friggin-V <williamventura711@gmail.com>
2023-06-27 15:28:04 -0400

    added port

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ app.get('/profile', (req, res) => { profile.handleProfile(req, res, db)});
 
 app.put('/image', (req, res) => { image.handleImage(req, res, db)});
 
-app.listen(3000, ()=>{
-    console.log('app is running on port 3000')
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log(`app is running on port ${process.env.PORT}`)
 })
 /*
 
